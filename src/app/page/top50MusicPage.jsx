8d9fdd35710693f50b2a78286c86f86c0bf0cc0b
darkside72fx <hoangtuathat@gmail.com>
2022-09-11 15:23:34 +0700

    up

---
+++ src/app/page/top50MusicPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Top50MusicPage = () => {
+  return (
+    <div className=" transition ease-in-out delay-1800 text-white">
+      <div>
+        <span className="text-5xl font-bold">
+          Top 50 bài hát khán giả nghe nhiều{" "}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Top50MusicPage;
